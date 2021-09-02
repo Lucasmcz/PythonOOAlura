00bfc40658df20e86f9e4d7aa55dcf41a0364fa3
Lucas Vinicius <88403508+Lucasmcz@users.noreply.github.com>
2021-09-02 08:16:42 -0300

    Update Extrator_url.py

--- Strings/Extrator_url.py
+++ Strings/Extrator_url.py
@@ -1,13 +1,38 @@
+import re
+'''
+Verifica se a base da URL está de acordo com o padrão correto.
+Exemplos de URLs válidas:
+    bytebank.com/cambio
+    bytebank.com.br/cambio
+    www.bytebank.com/cambio
+    www.bytebank.com.br/cambio
+    http://www.bytebank.com/cambio
+    http://www.bytebank.com.br/cambio
+    https://www.bytebank.com/cambio
+    https://www.bytebank.com.br/cambio
+Exemplos de URL inválidas:
+    https://bytebank/cambio
+    http://bytebank.naoexiste/cambio
+    ht:bytebank.naoexiste/cambio
+'''
 class Extrator_url():
     def __init__(self,url):
-        self.url = url.strip()
+        self.url = self.sanitiza_url(url)
         self.valida_url()
     def sanitiza_url(self,url):
-        return url.strip()
-
+        if type(url) == str:
+            return url.strip()
+        else:
+            return ""
     def valida_url(self):
-        if self.url == "":
+        if not self.url:
             raise ValueError("A URL Está vazia ")
+        padrao_url = re.compile('(http(s)?://)?(www.)?bytebank.com?(.br)?/cambio')
+        padrao_url.match('.......')
+        match = padrao_url.match(self.url)
+
+        if not match:
+            raise ValueError("A URL não é valida")
 
     def get_url_base(self):
         indice_interrogação = self.url.find("?")
@@ -18,6 +43,9 @@ class Extrator_url():
         indice_interrogação = self.url.find("?")
         parametros = self.url[indice_interrogação+1:]
         return parametros
+
+    def __len__(self):
+        return len(self.url)
 
     def valor_parametro(self):
         indice_interrogação = self.url.find("?")
@@ -33,10 +61,14 @@ class Extrator_url():
         else:
             valor = self.url[indice_valor:indicedoe]
         return valor
-url_youtube = Extrator_url("youtube.com/watch?v=QArog-oWuDk&t=3s")
+url_youtube = Extrator_url("http://www.bytebank.com.br/cambio")
+print(url_youtube.__len__())
+
+'''
 base = url_youtube.get_url_base()
 print(base)
 parametros = url_youtube.get_url_parametros()
 print(parametros)
 v = url_youtube.valor_parametro()
 print(v)
+'''
